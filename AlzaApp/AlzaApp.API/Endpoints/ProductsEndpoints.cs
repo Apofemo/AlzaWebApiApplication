2b0e53c4a826d2cbb78863736feac6029fdf12da
Aleksei Gaas <aleksei.gaas@consultant.lego.com>
2024-11-02 16:48:28 +0100

    Versioning implemented

--- AlzaApp/AlzaApp.API/Endpoints/ProductsEndpoints.cs
+++ AlzaApp/AlzaApp.API/Endpoints/ProductsEndpoints.cs
@@ -6,31 +6,31 @@ public static class ProductsEndpoints
 {
     public static void MapProductEndpoints(this IEndpointRouteBuilder app)
     {
-        app.MapGet("api/products", async (IProductsService productService) =>
+        app.MapGet("products", async (IProductsService productService) =>
         {
             var result = await productService.GetAllProductsAsync();
             
             return result.IsSuccess 
                 ? Results.Ok(result.Value) 
                 : Results.NotFound(result.Errors.First().Message);
-        });
+        }).MapToApiVersion(1);
 
-        app.MapGet("api/products/{id}", async (IProductsService productService, int id) =>
+        app.MapGet("products/{id}", async (IProductsService productService, int id) =>
         {
             var result = await productService.GetProductByIdAsync(id);
             
             return result.IsSuccess 
                 ? Results.Ok(result.Value) 
                 : Results.NotFound(result.Errors.First().Message);
-        });
+        }).MapToApiVersion(1);
 
-        app.MapPatch("api/products/{id}", async (IProductsService productService, int id, string description) =>
+        app.MapPatch("products/{id}", async (IProductsService productService, int id, string description) =>
         {
             var result = await productService.UpdateDescriptionAsync(id, description);
             
             return result.IsSuccess 
                 ? Results.Ok(result.Value) 
                 : Results.BadRequest(result.Errors.First().Message);
-        });
+        }).MapToApiVersion(1);
     }
 }
